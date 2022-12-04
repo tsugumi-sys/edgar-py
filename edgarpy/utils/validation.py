c44994b16443a884d63063e9bf3cc21ed62965dd
tsugumi-sys <tidemark0105@gmail.com>
2022-12-04 15:02:47 +0900

    casdca

--- edgarpy/utils/validation.py
+++ edgarpy/utils/validation.py
@@ -3,7 +3,6 @@ import posixpath
 
 def path_not_unique(path: str) -> bool:
     norm_path = posixpath.normpath(path)
-    print(path, norm_path)
     return (
         path != norm_path
         or norm_path == "."
--- tests/utils/test_validation.py
+++ tests/utils/test_validation.py
@@ -4,9 +4,8 @@ from edgarpy.utils.validation import path_not_unique
 
 
 def test_path_not_unique():
-    assert path_not_unique("a/b/c") == False
-    assert path_not_unique("./a/b/c") == True
-    assert path_not_unique("./a/b/../d") == True
+    assert path_not_unique("aa.bb/cc..d") == False
+    assert path_not_unique("./a/b/c../d") == True
     assert path_not_unique(".") == True
     assert path_not_unique("../a/b/c") == True
     assert path_not_unique("/a/b/c") == True
